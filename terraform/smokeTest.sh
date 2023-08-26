a7245ed86ed21264c67c444a218a0b453f00d986
SherifEmad20 <sherif.emad1091@gmail.com>
2023-08-26 18:06:35 +0300

    edit in smoke test script

--- terraform/smokeTest.sh
+++ terraform/smokeTest.sh
@@ -4,7 +4,7 @@ cd ~/SpringApp/terraform
 
 echo "Waiting for application to run..."
 
-sleep 10
+sleep 120
 
 # Get the public IP of the AWS EC2 instance
 public_ip=$(terraform output -raw server_ip)
@@ -12,7 +12,11 @@ public_ip=$(terraform output -raw server_ip)
 echo "Public IP of the EC2 instance: $public_ip"
 
 # Curl to the public IP
-curl_result=$(curl "http://$public_ip:8080/api/course/getCourses")
+while true; do
+    curl_result=$(curl "http://$public_ip:8080/api/course/getCourses") && break  # Break the loop on successful connection
+    sleep 120
+done
+
 
 # Display the result
 echo "Curl Result:"
